deb8a95832945a5a4bcd7c47c3dd33f3ac4891a5
siboles <scott.sibole@gmail.com>
2021-08-20 15:37:43 -0600

    Updated to test combinations of noise additions - 3 levels of each type.

--- src/testing/validation_synthetic_shot_noise.py
+++ src/testing/validation_synthetic_shot_noise.py
@@ -1,24 +1,21 @@
 import generateImages
 from resonant_lsm import segmenter
 
-# gaussian_noise_levels = [0.0, 0.1, 0.2]
-# shot_noise_levels = [0.0, 0.1, 0.2, 0.3]
-gaussian_noise_levels = [0.1]
-shot_noise_levels = [0.0]
+gaussian_noise_levels = [0.0, 0.1, 0.2]
+shot_noise_levels = [0.0, 0.1, 0.2]
 for gn in gaussian_noise_levels:
     for sn in shot_noise_levels:
         out_directory = 'gn_{:2.1f}_sn_{:2.1f}'.format(gn, sn).replace('.', 'p')
         root, all_seeds = generateImages.generate_test_images(background_noise=gn,
-                                                              shot_noise=sn,
+                                                              speckle_noise=sn,
+                                                              spacing=0.1,
                                                               number=1,
                                                               deformed=0,
                                                               output=out_directory)
-        print(all_seeds['reference'][0])
         seg = segmenter(image_directory=root,
                         spacing=[0.1, 0.1, 0.1],
                         seed_points=all_seeds['reference'][0],
                         bounding_box=[100, 100, 100],
-                        levelset_smoothing_scale=0.0,
+                        levelset_smoothing_scale=0.1,
                         equalization_fraction=0.0)
         seg.execute()
-        seg.write_image_as_vtk(seg.levelsets[-1], 'levelset')
